4af68439e0521882e56c03d580258d3c2a586a0e
guowei <guowei@eatbeancar.com>
2020-03-10 17:37:54 +0800

    修改搜索结果页标题导航的搜索接口参数

--- pages/searchResults/searchResults.js
+++ pages/searchResults/searchResults.js
@@ -7,7 +7,20 @@ Page({
      */
     data: {
         searchValue:'',
-        active:0
+        active:0,
+        currentTitleId:'1018',
+        // 导航标题
+        // 默认为 1 即单曲, 取值意义: 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频, 1018: 综合
+        titleList:[
+            {id:'1018',title:'综合'},
+            {id:'1',title:'单曲'},
+            {id:'10',title:'专辑'},
+            {id:'100',title:'歌手'},
+            {id:'1000',title:'歌单'},
+            {id:'1004',title:'MV'},
+            {id:'1009',title:'电台'},
+            {id:'1010',title:'视频'}
+        ]
     },
 
     /**
@@ -69,14 +82,35 @@ Page({
     onShareAppMessage: function () {
 
     },
+    // 搜索内容变化
+    searchChange(e){
+        console.log(e.detail)
+        var searchValue=e.detail
+        if(searchValue+"".trim()){
+            this.setData({
+                searchValue
+            })
+        }else{
+            return 
+        }
+        this.renderData(this.data.currentTitleId,this.data.searchValue)
+    },
     // tab切换
     onChange(e){
-
+        console.log(e.detail.index)
+        let currentIndex=e.detail.index
+        // 根据当前的index区查找对应的标题列表的id
+        var currentTitleId=this.data.titleList[currentIndex].id
+        this.setData({
+            currentTitleId
+        })
+        // 切换调用获取对应的内容(当前的titleId 关键字)
+        this.renderData(this.data.currentTitleId,this.data.searchValue)
     },
     // 默认为 1 即单曲, 取值意义: 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频, 1018: 综合
-    renderData(type = 1,words){
+    renderData(type,words){
         wx.request({
-            url: app.url + "/search?keywords=" + words +" & type="+type,
+          url: app.url + "/search?keywords=" + words + " & type=" + type,
             success:res=>{
                 console.log(res.data.result)
             }
